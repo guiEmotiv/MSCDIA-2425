d8480749b3e588b4709c56b3a892c3e98327b59e
guiEmotiv <guillermo.soto@utec.edu.pe>
2024-06-06 13:15:44 -0500

    v3

--- IA fundamentals/Tareas/GA TSP.py
+++ IA fundamentals/Tareas/GA TSP.py
@@ -20,26 +20,27 @@ distances = squareform(pdist(cities, 'euclidean'))
 """ RANDOM INITIALIZATION """
 def create_initial_population(pop_size: int, num_cities: int) -> list:
     population = []
-
     for _ in range(pop_size):
         individual = list(np.random.permutation(num_cities))
         population.append(individual)
-
         #print('Initial population:', individual)
-
     print(type(population))
 
     return population
 
 def fitness(solution: list) -> float:
     distance = 0
-
     for i in range(len(solution)-1):
         distance += distances[solution[i], solution[i+1]]
-
     return distance
 
 population = create_initial_population(pop_size, N)
 print(population)
 Plotting.plot_tsp(cities, population[0])
 print(fitness(population[0]))
+
+def roulette_wheel_selection(population: list, all_fitness: list) -> list:
+    total_fitness = sum(all_fitness)
+    selection_probs = [f/total_fitness for f in all_fitness]
+
+    return population[np.random.choice(len(population), p=selection_probs)]
